50cd83a248b8b62e9ab7970fc27086a2996b83bc
Lines3 <sporsheim.line@gmail.com>
2025-09-13 18:54:50 +0200

    Updates to Vitests, all tests ran successfully

--- js/utils/userInterface.test.js
+++ js/utils/userInterface.test.js
@@ -4,31 +4,27 @@ import { isActivePath } from "./userInterface.js";
 describe("Testing isActivePath function", () => {
   it("checks if the current path matches href exactly", () => {
     const href = "/";
-    const currentPath = window.location.pathname;
+    const currentPath = href;
     const result = isActivePath(href, currentPath);
     expect(result).toBe(true);
   });
   it("checks if root path (“/”) when path is “/” or “/index.html", () => {
-    const isCurrentPath = "/";
-    const href = window.location.pathname;
-    isActivePath(href);
-    expect(href).toBe(isCurrentPath);
+    const rootPath = "/";
+    const href = "/";
+    const index = "/index.html";
+    expect(isActivePath(rootPath, href)).toBe(true);
+    expect(isActivePath(rootPath, index)).toBe(true);
   });
   it("checks if the current path includes the href", () => {
-    const isCurrentPath = "/";
-    const href = window.location.pathname;
-    isActivePath(href);
-    expect(href).toBe(isCurrentPath);
+    const currentPath = "/signup.html";
+    const href = "/signup.html";
+    const result = isActivePath(href, currentPath);
+    expect(result).toBe(true);
   });
   it("checks if path don't match and returns false", () => {
     const CurrentPath = "/signup.html";
-    const href = window.location.pathname;
+    const href = "/";
     const result = isActivePath(href, CurrentPath);
-    expect(result).not.toBe(false);
+    expect(result).toBe(false);
   });
 });
-
-// Returns true when current path matches href exactly
-// Returns true for root path (“/”) when path is “/” or “/index.html”
-// Returns true when current path includes the href
-// Returns false when paths don’t match
